102643f6f998b7a932a3e4c3d7e14ab0cdfaa283
Owen <owenou666@gmail.com>
2019-05-09 16:36:27 -0400

    task number 8

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+        var apiBase = "https://jsonplaceholder.typicode.com/photos/";
+        var orderID = 0;
+
+        axios.get(apiBase).then(function(response){
+            $('#showPicture').on('click',function(){
+                orderID++;
+                var url= apiBase+orderID;
+
+                axios.get(url).then(function(response){
+                    
+                    setTimeout(function(){
+                        var title = response.data.title;
+                        var imgs = response.data.url;
+                        var thumbnailUrl = response.data.thumbnailUrl;
+
+                        var $imgs = $('<img>').attr("src",imgs).attr("alt",url);
+                        var $title = $('<p>').html('title: '+title);
+                        var $thumbnailUrl = $('<p>').html('thumbnailUrl: '+ thumbnailUrl);
+
+                        $('#task').html([$imgs,$title,thumbnailUrl]);
+                        $('#numberCounter').html('Now, the current picture is number: ' + orderID);
+                        $('#display').html(' Please keep click for next pictures ');
+                },3000);   
+            });
+            $('#display').html(' We are searching your next picture, please wait... ');
+        });
+    });
+});
